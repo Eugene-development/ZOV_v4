6abfee0caf9b5a75efc7ed08852c3b5ac3d77f09
Eugene Che <indexpro24@gmail.com>
2023-06-04 16:30:10 +0300

    c

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -10,7 +10,11 @@ import Footer from "./z-module/layout/Footer";
 const inter = Inter({ subsets: ['latin'] })
 
 
-export default function RootLayout({children}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="ru">
       <head>
@@ -34,7 +38,6 @@ export default function RootLayout({children}) {
           />
         <noscript><div><img src="https://mc.yandex.ru/watch/93835019" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
       </head>
-
       <body className={inter.className}>
         <Banner/>
         <Header />
