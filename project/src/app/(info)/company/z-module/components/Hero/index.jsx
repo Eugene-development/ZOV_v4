778b007de99186e7663b0ba0476e0cca195d2365
Eugene Che <indexpro24@gmail.com>
2023-06-04 11:07:08 +0300

    c

--- project/src/app/(info)/company/z-module/components/Hero/index.jsx
+++ project/src/app/(info)/company/z-module/components/Hero/index.jsx
@@ -41,14 +41,17 @@ export default function Hero (){
             <div className="mx-auto max-w-7xl px-6 pb-16 pt-8 sm:pt-24 lg:px-8 lg:pt-12">
               <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                 <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-                  <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    Мебель от фабрики <br/>для вашей семьи
+                  <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                    Мебель <br/>для вашей семьи
                   </h1>
                   {/* <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                     Мы официальный дилер фабрики "ЗОВ" <br/>в России
                   </h1> */}
-                  <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                    Наша компания располагает самой крупной сетью мебельных салонов фабрики "ЗОВ" в России. Предлагаем Вам отличный сервис, доступные цены на мебель премиального класса, а также дружелюбное отношение нашего персонала &#128578;. Ждём Вас в наших салонах!
+                  <p className="relative mt-6 text-base sm:text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+                    Наша компания располагает самой крупной сетью мебельных салонов фабрики "ЗОВ" в России. Предлагаем Вам отличный сервис, доступные цены на мебель премиального класса, а также дружелюбное отношение нашего персонала &#128578;.
+                  </p>
+                  <p className="relative mt-6 text-base sm:text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+                    Ждём Вас в наших салонах!
                   </p>
                 </div>
                 <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
@@ -98,7 +101,7 @@ export default function Hero (){
                       />
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                     </div>
-                    <div className="relative">
+                    <div className="hidden md:block relative">
                       <img
                         src="https://avatars.mds.yandex.net/i?id=90ee170d38948c7d40205eaf68d5f03a52468fb7-8270623-images-thumbs&n=13"
                         alt=""
