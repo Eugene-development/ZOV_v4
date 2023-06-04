2ed7b8b402edec6a544889573d6b188263eab544
Eugene Che <indexpro24@gmail.com>
2023-06-04 16:16:05 +0300

    c

---
+++ project/src/app/layout.jsx
@@ -0,0 +1,46 @@
+// import './globals.css'
+import '../../src/css/globals.css'
+import { Inter } from 'next/font/google'
+
+import Banner from "./z-module/layout/Banner";
+import Header from "./z-module/layout/Header";
+import Footer from "./z-module/layout/Footer";
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default function RootLayout({children}) {
+  return (
+    <html lang="ru">
+      <head>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();
+                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                        ym(93835019, "init", {
+                              clickmap:true,
+                              trackLinks:true,
+                              accurateTrackBounce:true,
+                              webvisor:true
+                        });
+                      `
+            }}
+          />
+        <noscript><div><img src="https://mc.yandex.ru/watch/93835019" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+      </head>
+      
+      <body className={inter.className}>
+        <Banner/>
+        <Header />
+          {children}
+        <Footer/>
+      </body>
+    </html>
+  )
+}
