8163fe598158b1a362ac46a64674f66e0fafb58f
Eugene Che <indexpro24@gmail.com>
2023-06-06 19:17:57 +0300

    с

--- project/src/app/(production)/catalog/kuhna/page.jsx
+++ project/src/app/(production)/catalog/kuhna/page.jsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <>
         <Head/>
-        <Filter/>
+        {/* <Filter/> */}
         <ProductList/>
     </>
   )
--- project/src/app/(production)/catalog/kuhna/z-module/components/Head/index.jsx
+++ project/src/app/(production)/catalog/kuhna/z-module/components/Head/index.jsx
@@ -7,7 +7,8 @@ export default function Head() {
       <div className="bg-white py-8 sm:py-20">
           <div className="mx-auto max-w-screen-sm text-center">
               <h2 className="mb-4 text-2xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Кухонные гарнитуры</h2>
-              <p className="px-2 font-light text-gray-500 text-sm sm:text-xl dark:text-gray-400">Каталог кухонных гарнитуров различной конфигурации и цвета</p>
+              <p className="px-2 font-light text-gray-500 text-sm sm:text-xl dark:text-gray-400">Выберите подходящую для вас конфигурацию, стиль и цвет кухонного гарнитура</p>
+              {/* <p className="px-2 font-light text-gray-500 text-sm sm:text-xl dark:text-gray-400">Выберите с помощью фильтра подходящую конфигурацию, стиль и цвет кухонного гарнитура</p> */}
           </div> 
       </div> 
     </motion.main>
