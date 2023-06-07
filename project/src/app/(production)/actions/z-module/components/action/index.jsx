cc67a600e035b9bb566a22ef8776bc4facb5403b
Eugene Che <indexpro24@gmail.com>
2023-06-07 21:16:59 +0300

    с

--- project/src/app/(production)/actions/z-module/components/action/index.jsx
+++ project/src/app/(production)/actions/z-module/components/action/index.jsx
@@ -25,44 +25,40 @@ export default function Action() {
   return (
       <div className="overflow-hidden bg-white py-8 sm:py-20">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-
-          
-    <motion.main 
-      initial={{ opacity: 0 }} 
-      animate={{ opacity: 2, y: -10 }} 
-      transition={{ type: "spring", stiffness: 50 }}
-    >
-
-          <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-                  <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Акции и подарки</h2>
-                  <p className="font-light text-gray-500 text-sm sm:text-xl dark:text-gray-400">Мы стараемся Вас порадовать не только качественной и красивой мебелью, но и приятными бонусами при покупке!</p>
+          <div className="mx-auto max-w-screen-sm text-center mb-4 lg:mb-8">
+              <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Акции и подарки</h2>
+              <p className="font-light text-gray-500 text-sm sm:text-xl dark:text-gray-400">Мы стараемся Вас порадовать не только качественной и красивой мебелью, но и приятными бонусами при покупке!</p>
           </div> 
-    </motion.main>
-
-          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div className="lg:pr-8 lg:pt-4">
-              <div className="lg:max-w-lg">
-                <dl className="mt-10 max-w-xl space-y-8  leading-7 text-gray-600 lg:max-w-none">
-                  {features.map((feature) => (
-                    <div key={feature.name} className="relative pl-12 pt-1">
-                      <dt className="inline text-lg font-semibold text-gray-900">
-                        <feature.icon className="absolute left-1 top-1 h-7 w-7 text-indigo-400" aria-hidden="true" />
-                        "{feature.name}"
-                      </dt>{' '}<br/>
-                      <dd className="inline text-base">{feature.description}</dd>
-                    </div>
-                  ))}
-                </dl>
+          <motion.main 
+            initial={{ opacity: 0 }} 
+            animate={{ opacity: 2, y: -10 }} 
+            transition={{ type: "spring", stiffness: 50 }}
+          >
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+              <div className="lg:pr-8 lg:pt-4">
+                <div className="lg:max-w-lg">
+                  <dl className="mt-10 max-w-xl space-y-8  leading-7 text-gray-600 lg:max-w-none">
+                    {features.map((feature) => (
+                      <div key={feature.name} className="relative pl-12 pt-1">
+                        <dt className="inline text-lg font-semibold text-gray-900">
+                          <feature.icon className="absolute left-1 top-1 h-7 w-7 text-indigo-400" aria-hidden="true" />
+                          "{feature.name}"
+                        </dt>{' '}<br/>
+                        <dd className="inline text-base">{feature.description}</dd>
+                      </div>
+                    ))}
+                  </dl>
+                </div>
               </div>
+              <img
+                src="https://art-holding.su/image/catalog/category/gifts2.jpg"
+                alt="Product screenshot"
+                className="w-[40rem] max-w-none  sm:w-[50rem]"
+                width={2432}
+                height={1442}
+              />
             </div>
-            <img
-              src="https://art-holding.su/image/catalog/category/gifts2.jpg"
-              alt="Product screenshot"
-              className="w-[40rem] max-w-none  sm:w-[50rem]"
-              width={2432}
-              height={1442}
-            />
-          </div>
+          </motion.main>
         </div>
       </div>
   )
