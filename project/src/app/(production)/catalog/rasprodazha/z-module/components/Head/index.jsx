e92fde3a98bdcd598608a9b1eb29a7e686c65c4b
Eugene Che <indexpro24@gmail.com>
2023-06-08 22:12:29 +0300

    с

--- project/src/app/(production)/catalog/rasprodazha/z-module/components/Head/index.jsx
+++ project/src/app/(production)/catalog/rasprodazha/z-module/components/Head/index.jsx
@@ -6,8 +6,8 @@ export default function Head() {
     <motion.main initial={{ opacity: 0 }} animate={{ opacity: 2 }} >
       <div className="bg-white py-8 sm:py-20">
           <div className="mx-auto max-w-screen-sm text-center">
-              <h2 className="mb-4 text-2xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Кухонные гарнитуры</h2>
-              <p className="px-2 font-light text-gray-500 text-sm sm:text-xl dark:text-gray-400">Выберите подходящую для вас конфигурацию, стиль и цвет кухонного гарнитура</p>
+              <h2 className="mb-4 text-2xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Распродажа мебели</h2>
+              <p className="px-2 font-light text-gray-500 text-sm sm:text-xl dark:text-gray-400">Продаём образцы мебели из салонов по сниженным ценам в связи с обновлением экспозиций</p>
               {/* <p className="px-2 font-light text-gray-500 text-sm sm:text-xl dark:text-gray-400">Выберите с помощью фильтра подходящую конфигурацию, стиль и цвет кухонного гарнитура</p> */}
           </div> 
       </div> 
