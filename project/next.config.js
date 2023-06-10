a9ab43e2163500c2a69b41187622f597fed88f93
Eugene Che <indexpro24@gmail.com>
2023-06-10 21:24:51 +0300

    c

--- project/next.config.js
+++ project/next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   env: {
     customKey: "my-value",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://avatars.dzeninfra.ru",
+        port: "",
+        pathname:
+          "/get-zen_doc/9707955/pub_646a10017bd61e597f2efc50_646a100729bffd1d35f84376/",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
---
+++ project/src/app/(content)/post/1/z-module/components/Article/index.jsx
@@ -0,0 +1,7 @@
+import PostComponent from '@/UI/Post'
+
+export default ({ param }) =>{
+  return (
+    <PostComponent/>
+  )
+}
