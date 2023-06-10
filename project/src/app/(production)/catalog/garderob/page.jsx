f0bacbc416661cc68ac15469e3bdf99e5c76540e
Eugene Che <indexpro24@gmail.com>
2023-06-10 23:19:12 +0300

    c

--- project/src/app/(production)/catalog/garderob/page.jsx
+++ project/src/app/(production)/catalog/garderob/page.jsx
@@ -1,10 +1,39 @@
-import Head from "./z-module/components/Head"
-import Filter from "./z-module/components/Filter/index"
-import ProductList from "./z-module/components/ProductList"
+import Head from "@/app/(production)/z-module/components/Head"
+import ProductList from "@/app/(production)/z-module/components/ProductList"
 
 export const metadata = {
   title: 'Фабрика ЗОВ | Гардеробы',
   description: 'Гардеробы от фабрики ЗОВ',
+}
+
+const data = {
+  products: [
+    {
+      project: "ШКАФ 1. BASIC LINE",
+      img: 'https://zovofficial.com/image/catalog/products/garderobnye-shkafy-i-sistemy/garderobnyj-sh-kaf-1-basic-line-ru/garderobnyj-sh-kaf-1-basic-line-ru_49206.jpeg'
+    },
+    {
+      project: "ШКАФ 7. SMART LINE",
+      img: 'https://zovofficial.com/image/catalog/products/garderobnye-shkafy-i-sistemy/garderobnyj-sh-kaf-liniya-smart-wl-elite-wl-ru/garderobnyj-sh-kaf-liniya-smart-wl-elite-wl-ru_49123.jpeg'
+    },
+    {
+      project: "ШКАФ 3. BASIC LINE",
+      img: 'https://zovofficial.com/image/catalog/products/garderobnye-shkafy-i-sistemy/sh-kaf-liniya-basic-ru/sh-kaf-liniya-basic-ru_49110.jpeg'
+    },
+    {
+      project: "ШКАФ 4. BASIC / SMART LINE",
+      img: 'https://zovofficial.com/image/catalog/products/garderobnye-shkafy-i-sistemy/garderobnyj-sh-kaf-1-basic-smart-line-ru/garderobnyj-sh-kaf-1-basic-smart-line-ru_49189.jpeg'
+    },
+    {
+      project: "ШКАФ 18. BASIC / SMART LINE",
+      img: 'https://zovofficial.com/image/catalog/products/garderobnye-shkafy-i-sistemy/2023/garderob-18/1.jpg'
+    },
+    {
+      project: "ШКАФ 17. BASIC / SMART LINE",
+      img: 'https://zovofficial.com/image/catalog/products/garderobnye-shkafy-i-sistemy/2023/garderob-17/cam-2.jpg'
+    },
+]
+
 }
 
 export default function Page() {
@@ -12,7 +41,7 @@ export default function Page() {
     <>
         <Head/>
         {/* <Filter/> */}
-        <ProductList/>
+        <ProductList products={data.products}/>
     </>
   )
 }
