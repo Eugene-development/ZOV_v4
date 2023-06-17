efdfd72603dae3a75930dc7ed9554c9d29e1cf7c
Eugene Che <indexpro24@gmail.com>
2023-06-17 17:59:52 +0300

    c

--- project/src/app/(content)/z-module/components/Article/index.jsx
+++ project/src/app/(content)/z-module/components/Article/index.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { motion } from "framer-motion"
+import CopyLink from "../CopyLink"
  
 export default (props) => {
   return (
@@ -9,7 +10,7 @@ export default (props) => {
 
             <header className="py-12">
                 <div className="px-4 mx-auto w-full max-w-screen-xl text-center">
-                    <span className="block mb-4 font-semibold text-gray-900 dark:text-white">Опубликовано <time className="font-normal text-gray-500 dark:text-gray-400 uppercase"   dateTime={props.data.date} title={props.data.date}>{props.data.date}</time></span>
+                    <span className="block mb-4 font-semibold text-gray-900 dark:text-white">Опубликовано {' '} <time className="font-normal text-gray-500 dark:text-gray-400"   dateTime={props.data.date} title={props.data.date}>{props.data.date}</time></span>
                     <h1 className="font-[lato] mx-auto mb-4 max-w-3xl text-2xl dark:text-white font-extrabold leading-none text-gray-900 sm:text-3xl lg:text-4xl">Как выбрать идеальный цвет фасадов, каркаса и столешницы для вашего кухонного гарнитура</h1>
                     {/* <p className="text-lg font-normal text-gray-500 dark:text-gray-400">Мы рады поделиться с Вами своим опытом и помочь в выборе мебели нашей фабрики.</p> */}
                 </div>
@@ -34,14 +35,16 @@ export default (props) => {
                                     <path
                                         d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
                                 </svg> Tweet</a> */}
-                            <button type="button"
+                            {/* <button type="button"
                                 className="inline-flex items-center py-2 px-6 text-xs font-medium text-gray-900 no-underline bg-white rounded-lg border border-gray-200 focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"><svg
                                     className="mr-2 w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path fillRule="evenodd"
                                         d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
                                         clipRule="evenodd"></path>
-                                </svg> Скопировать ссылку</button>
+                                </svg> Скопировать ссылку
+                            </button> */}
+<CopyLink />
                         </aside>
                     </div>
 
---
+++ project/src/app/(content)/z-module/components/CopyLink/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+function CopyButton() {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      setCopied(true);
+    } catch (err) {
+      console.error('Не удалось скопировать ссылку: ', err);
+    }
+  };
+
+  return (
+    <div>
+        {!copied && 
+            <button type="button" onClick={copyToClipboard}
+                className="inline-flex items-center py-2 px-6 text-xs font-medium text-gray-900 no-underline bg-white rounded-lg border border-gray-200 focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"><svg
+                    className="mr-2 w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd"
+                        d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                        clipRule="evenodd"></path>
+                </svg> Скопировать ссылку
+            </button>
+        }
+        
+      {copied && 
+            <div 
+                className="inline-flex items-center py-2 px-6 text-xs font-medium text-gray-900 no-underline bg-white rounded-lg border border-gray-200 focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"><svg
+                    className="mr-2 w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd"
+                        d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                        clipRule="evenodd"></path>
+                </svg> Ссылка скопирована
+            </div>
+        }
+    </div>
+  );
+}
+
+export default CopyButton;
