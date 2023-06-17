299191d02552f1c1702f34817ab430000fa4b894
Eugene Che <indexpro24@gmail.com>
2023-06-17 19:00:45 +0300

    с

--- project/src/app/(info)/contacts/z-module/components/Addresses/index.jsx
+++ project/src/app/(info)/contacts/z-module/components/Addresses/index.jsx
@@ -122,6 +122,102 @@ const showrooms = [
         img: 'https://zovrus.ru/files/_cache/salon/mg_8425.resize1000x1000.jpg?b65a57269bf81082f214ef1ea68f54aa',
         link: '/'
     },
+    {
+        city: 'Москва',
+        address: 'ШОССЕ ЭНТУЗИАСТОВ, 76/1',
+        metro: 'Перово',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/_mg_6827.resize1000x1000.jpg?40a066075feb9a7cb42564a6763d72e3',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'БОЛЬШАЯ ЧЕРКИЗОВСКАЯ УЛИЦА, 11',
+        metro: 'Преображенская пл.',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/chr-2183.crop596x380.jpg?0bfd5e9120d5aab3b6063d99b0f8e1f3',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'МКАД, 23-Й КМ., КИЕВСКОЕ ШОССЕ, 8Г',
+        metro: 'Румянцево',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/family-room.crop596x380.png?0ccd953d66713e33cbaa239d3437f708',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'ПР. ВЕРНАДСКОГО, 86А',
+        metro: 'Юго-Западная',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/avenu.crop596x380.jpg?02792ab35aa7c3009ea23fdccc931186',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'ЛЕНИНГРАДСКИЙ ПРОСПЕКТ 74К1',
+        metro: 'Сокол',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/chr-4935_1.crop596x380.jpg?7cce1e5107c4b24c8f03d9f2903235d2',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'ЛУГОВАЯ УЛ., 1, РАБОЧИЙ ПОСЁЛОК НОВОИВАНОВСКОЕ',
+        metro: 'ТЦ Три Кита',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/chr-0474.crop596x380.jpg?0cda281955b6d055fbf117f7e38feda8',
+        link: '/'
+    },
+    {
+        city: 'Видное',
+        address: 'ОЛИМПИЙСКАЯ УЛИЦА, 1К2',
+        metro: 'Видное',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/bez-imeni-1.crop596x380.jpg?4e2ac885ea65888c7533f090a4596a07',
+        link: '/'
+    },
+    {
+        city: 'Реутов',
+        address: 'ТРЦ ШОКОЛАД - МКАД, 2-Й КМ',
+        metro: 'Новокосино',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/_mg_8433.crop596x380.jpg?76bb68d31f6af24cc8c53704cdb8b142',
+        link: '/'
+    },
+    {
+        city: 'Реутов',
+        address: 'ТРЦ ШОКОЛАД - МКАД, 2-Й КМ',
+        metro: 'Новокосино',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/_mg_8433.crop596x380.jpg?76bb68d31f6af24cc8c53704cdb8b142',
+        link: '/'
+    },
+    {
+        city: 'Химки',
+        address: 'МОЛОДЁЖНАЯ УЛИЦА, 78',
+        metro: '',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/chr-141210.crop596x380.jpg?6c5e18028db8f92629e1491869f322e0',
+        link: '/'
+    },
+    {
+        city: 'Домодедово',
+        address: 'КАШИРСКОЕ ШОССЕ 17А, ТЦ «ГАЛЕРЕЯ»',
+        metro: '',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/img_20200716_162443.crop596x380.jpg?3717a8c213952bcbd594e84139b670f3',
+        link: '/'
+    },
+    {
+        city: 'Троицк',
+        address: 'АКАДЕМИЧЕСКАЯ ПЛОЩАДЬ, 3',
+        metro: '',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/img_20221107_110957.crop596x380.jpg?98e3b9cd86cf5e3372acadf7028b7203',
+        link: '/'
+    },
 ]
 
 export default function Addresses() {
