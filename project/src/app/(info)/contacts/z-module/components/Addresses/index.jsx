b8f13f81a3a9e0417123ccf543673ffff64e66d9
Eugene Che <indexpro24@gmail.com>
2023-06-17 18:45:38 +0300

    c

--- project/src/app/(info)/contacts/z-module/components/Addresses/index.jsx
+++ project/src/app/(info)/contacts/z-module/components/Addresses/index.jsx
@@ -61,9 +61,65 @@ const showrooms = [
     {
         city: 'Москва',
         address: 'Бутырская улица, 65/68',
-        metro: 'Бульвар Дмитрия Донского',
-        workingMode: '11.00-21.00',
+        metro: 'Дмитровская',
+        workingMode: '1.00-21.00',
         img: 'https://zovrus.ru/files/_cache/salon/_mg_7879.resize1000x1000.jpg?6e3e67c77f4b7ce4729655a9f2fb023c',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'МКАД, 24-Й КИЛОМЕТР, ТЦ ТВОЙ ДОМ',
+        metro: 'Домодедовская',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/chr-6676.resize1000x1000.jpg?d998c197bfa57fb655b21c3fdf3bad90',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'КАШИРСКОЕ ШОССЕ, 62/2',
+        metro: 'Каширская',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/hdr__mg_7989.resize1000x1000.jpg?7fffc1bb60047b7f85e4331f85fa1e43',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'НОВОРЯЗАНСКОЕ ШОССЕ, Д.3',
+        metro: 'Котельники',
+        workingMode: '10.00-20.00',
+        img: 'https://zovrus.ru/files/_cache/salon/2cecffe89917e3b87ef2741073b70f1b.resize1000x1000.jpg?585036492f616f8e03b37f7a9c400bdd',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'МОЖАЙСКОЕ ШОССЕ, 25',
+        metro: 'Кунцевская',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/chr-5922.resize1000x1000.jpg?e43df2242c9a949890c1d4529f481e8a',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'УЛ. ОБРУЧЕВА, 34/63, СТР.1',
+        metro: 'Калужская',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/1bef1c6bdace78a633d444cfd76396e8.resize1000x1000.jpeg?50e08249b3432a375625e2665cb2b220',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'НОВОУХТОМСКОЕ ШОССЕ, Д. 2А, ТЦ ГОРОД КОСИНО',
+        metro: 'Косино',
+        workingMode: '10.00-22.00',
+        img: 'https://zovrus.ru/files/_cache/salon/chr-5068.resize1000x1000.jpg?346dd7aaeae38e90bc69c09f97716b2d',
+        link: '/'
+    },
+    {
+        city: 'Москва',
+        address: 'ЛЮБЛИНСКАЯ УЛИЦА, 165',
+        metro: 'Марьино',
+        workingMode: '10.00-21.00',
+        img: 'https://zovrus.ru/files/_cache/salon/mg_8425.resize1000x1000.jpg?b65a57269bf81082f214ef1ea68f54aa',
         link: '/'
     },
 ]
@@ -80,12 +136,12 @@ export default function Addresses() {
                 <div className="grid gap-6 lg:gap-12 md:grid-cols-2 ">
                     {showrooms.map(item => (
                         <div key={item.address} className="flex flex-col xl:flex-row transition ease-in-out hover:scale-105 border-r">
-                                <img src={item.img} className="md:mr-5 max-w-xs rounded-md" alt="saloon"></img>
+                                <img src={item.img} className="md:mr-5 max-w-xs rounded-md object-fill h-52 w-96" alt="saloon"></img>
                             <div className="flex flex-col mt-2">
                                 <p className="mb-2 text-2xl font-bold leading-tight text-gray-900 dark:text-white">
                                     <span>г. {item.city},</span>
                                 </p>
-                                <p className="mb-2 text-lg font-bold leading-tight text-gray-900 dark:text-white">
+                                <p className="mb-2 text-sm font-bold leading-tight text-gray-900 dark:text-white uppercase">
                                     <span>{item.address}</span>
                                 </p>
                                 <p className="my-2 font-light text-gray-500 dark:text-gray-400 max-w-sm">Метро: "{item.metro}"</p>
