ed8fd00dc1bdd4b56a51e66e7aeccd74b96d0fe1
Eugene Che <indexpro24@gmail.com>
2023-06-18 21:48:54 +0300

    c

--- project/src/app/(info)/contacts/z-module/components/Map/index.jsx
+++ project/src/app/(info)/contacts/z-module/components/Map/index.jsx
@@ -1,10 +1,8 @@
 export default () => {
 
   return (
-      <div  className="my-8 p-2 bg-gray-50 mx-16 overflow-hidden sm:rounded-lg shadow-xl">
-        <div className="px-4 py-4 sm:p-6 ">
-          <iframe className="mx-auto" src="https://yandex.ru/map-widget/v1/?um=constructor%3Af9bee8bd6b122a14a8439697e72eb43a0de4a8b86048e498132f95d93fd53bd6&amp;source=constructor" width="727" height="572" frameborder="0"></iframe>
-        </div>
+      <div  className=" mb-16 overflow-hidden ">
+        <iframe className="px-4 md:px-20 pb-8 rounded-md" src="https://yandex.ru/map-widget/v1/?um=constructor%3Af9bee8bd6b122a14a8439697e72eb43a0de4a8b86048e498132f95d93fd53bd6&amp;source=constructor" width="100%" height="572" frameborder="0"></iframe>
       </div>
   )
 }
