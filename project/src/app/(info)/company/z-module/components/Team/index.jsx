5826d0a8e23788394b39a72fdf3318ed9eff8c37
Eugene Che <indexpro24@gmail.com>
2023-07-06 13:06:55 +0300

    c

--- project/src/app/(info)/company/z-module/components/Team/index.jsx
+++ project/src/app/(info)/company/z-module/components/Team/index.jsx
@@ -1,83 +1,58 @@
 const team = [
   {
-    name: 'Michael Fosterasasasas',
-    role: 'Co-Founder / CTO',
+    name: 'Александр Кайкы',
+    email: '204@zovrus.ru',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      'https://zovrus.ru/files/_cache/user/chr-3221.resize3000x3000.jpg?853cefe61157af152539d7d8a20d3e96',
   },
   {
-    name: 'Michael Fosteredcercerc',
-    role: 'Co-Founder / CTO',
+    name: '',
+    email: '@zovrus.ru',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      '',
   },
   {
-    name: 'Michael Foster233',
-    role: 'Co-Founder / CTO',
+    name: '',
+    email: '@zovrus.ru',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      '',
   },
   {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
+    name: '',
+    email: '@zovrus.ru',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      '',
   },
   {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
+    name: '',
+    email: '@zovrus.ru',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      '',
   },
   {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
+    name: '',
+    email: '@zovrus.ru',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      '',
   },
   {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
+    name: '',
+    email: '@zovrus.ru',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      '',
   },
   {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
+    name: '',
+    email: '@zovrus.ru',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      '',
   },
-  {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-  },
-  {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-  },
-  {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-  },
-  {
-    name: 'Michael Foster',
-    role: 'Co-Founder / CTO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-  },
-  // More people...
 ]
 
 export default () =>{
     return (
         <div className="mx-auto my-32 max-w-7xl px-6 sm:my-64 lg:px-8">
-          <div className="mx-auto max-w-3xl lg:mx-0">
+          <div className="mx-auto max-w-5xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Лучшие дизайнеры нашей компании</h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Наши дизайнеры - талантливые профессионалы, вдохновленные творчеством, они создают уникальные и функциональные мебельные решения, отражающие индивидуальный стиль и вкус наших клиентов.
@@ -89,9 +64,9 @@ export default () =>{
           >
             {team.map((person) => (
               <li key={person.name}>
-                <img className="mx-auto h-24 w-24 rounded-full" src={person.imageUrl} alt="" />
+                <img className="mx-auto h-36 w-24 rounded-md" src={person.imageUrl} alt="" />
                 <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
-                <p className="text-sm leading-6 text-gray-600">{person.role}</p>
+                <p className="text-sm leading-6 text-gray-600">{person.email}</p>
               </li>
             ))}
           </ul>
